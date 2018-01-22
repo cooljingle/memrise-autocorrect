99909891fed65c400ceaff591e4cfd37150eb13d
Aaron Miles <cool_jingle@hotmail.co.uk>
2018-01-22 12:26:59 +0000

    fix for memrise update

--- Memrise_Autocorrect.user.js
+++ Memrise_Autocorrect.user.js
@@ -4,7 +4,7 @@
 // @description    Corrects diacritics, punctuation and case while typing
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
-// @version        0.0.11
+// @version        0.0.12
 // @updateURL      https://github.com/cooljingle/memrise-autocorrect/raw/master/Memrise_Autocorrect.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-autocorrect/raw/master/Memrise_Autocorrect.user.js
 // @grant          none
@@ -133,7 +133,7 @@ $(document).ready(function () {
 
     function processInput (e) {
         var b = MEMRISE.garden.box,
-            p = b.testData.accepted,
+            p = b.testData.correct,
             i = b.$input.input || b.$input,
             v = i.val(),
             sliceIndex = 0;
